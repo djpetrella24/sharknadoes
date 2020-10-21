a92c54769d544df46ea3ba6479f014478350b547
Dave Damon <dave@daviddamon.com>
2020-10-21 18:16:24 -0400

    js updated

--- webpage/static/js/logic.js
+++ webpage/static/js/logic.js
@@ -1,53 +1,170 @@
-// Creating map object
-var myMap = L.map("map", {
-  center: [40.7, -73.95],
-  zoom: 11
-});
+//=======Map with Markers Starts Here ===================
 
-// Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+// // Creating map object
+// var myMap = L.map("map", {
+//   center: [40.7, -73.95],
+//   zoom: 11
+// });
 
-// // Store API query variables
-// var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-// var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-// var complaint = "&complaint_type=Rodent";
-// var limit = "&$limit=10000";
+// // Adding tile layer to the map
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(myMap);
 
-// // Assemble API query URL
-// var url = baseURL + date + complaint + limit;
+// // // Store API query variables
+// // var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
+// // var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
+// // var complaint = "&complaint_type=Rodent";
+// // var limit = "&$limit=10000";
 
-var queryData = "../../assets/data/dummydata.json"
+// // // Assemble API query URL
+// // var url = baseURL + date + complaint + limit;
 
-// Grab the data with d3
-d3.json(queryData, function(response) {
+// var queryData = "../../assets/data/dummydata.json"
 
-  // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
+// // Grab the data with d3
+// d3.json(queryData, function(response) {
 
-  // Loop through data
-  for (var i = 0; i < response.length; i++) {
+//   // Create a new marker cluster group
+//   var markers = L.markerClusterGroup();
 
-    // Set the data location property to a variable
-    var location = response[i].location;
+//   // Loop through data
+//   for (var i = 0; i < response.length; i++) {
 
-    // Check for location property
-    if (location) {
+//     // Set the data location property to a variable
+//     var location = response[i].location;
 
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(response[i].descriptor));
-    }
+//     // Check for location property
+//     if (location) {
 
+//       // Add a new marker to the cluster group and bind a pop-up
+//       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+//         .bindPopup(response[i].descriptor));
+//     }
+
+//   }
+
+//   // Add our marker cluster layer to the map
+//   myMap.addLayer(markers);
+
+// });
+
+
+
+
+
+// *****ADD D3.JSON******
+
+//========= Bar Chart Starts Here =================
+// attacks per year - add country filter?
+
+// basic bar chart
+var trace1 = {
+  x: ['giraffes', 'orangutans', 'monkeys'],
+  y: [20, 14, 23],
+    type: 'bar'
+};
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot('myDiv', data, layout);
+
+
+// horizontal bar chart 15.2.06
+// Sort the data by Greek search results
+var sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
+
+// Slice the first 10 objects for plotting
+slicedData = sortedByGreekSearch.slice(0, 10);
+
+// Reverse the array to accommodate Plotly's defaults
+reversedData = slicedData.reverse();
+
+// Trace1 for the Greek Data
+var trace1 = {
+  x: reversedData.map(object => object.greekSearchResults),
+  y: reversedData.map(object => object.greekName),
+  text: reversedData.map(object => object.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
   }
+};
 
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
 
-});
+
+
+//========= Pie Chart Starts Here =================
+
+// Create an array of each country's numbers
+var us = Object.values(data.us);
+var uk = Object.values(data.uk);
+var canada = Object.values(data.canada);
+
+// Create an array of music provider labels
+var labels = Object.keys(data.us);
+
+// Display the default plot
+function init() {
+  var data = [{
+    values: us,
+    labels: labels,
+    type: "pie"
+  }];
+
+  var layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.newPlot("pie", data, layout);
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data = [];
+
+  if (dataset == 'us') {
+      data = us;
+  }
+  else if (dataset == 'uk') {
+      data = uk;
+  }
+  else if (dataset == 'canada') {
+      data = canada;
+  }
+  // Call function to update the chart
+  updatePlotly(data);
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
